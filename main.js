67f25f63c7c04c22a5fcd376120fe1cb0cb2305b
GurnikVitaliy <111244081+GurnikVitaliy@users.noreply.github.com>
2022-08-27 10:45:35 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
-
 const accardion = document.querySelectorAll('.accardion__item');
 
 for(let accardions of accardion) {
-    accardions.addEventListener('click',function() {
-        if(accardions.classList.contains('active')){
-            accardions.classList.remove('active');
-        } else {
-            accardion.forEach(function(item) {
-                item.classList.remove('active');
-            })
-            accardions.classList.add('active');
-        }
+    accardions.addEventListener('click', function() {
+        accardions.classList.toggle('active');
     })
 }
