68efb76f3d645d61165c2681c4f657fd33bdf0c8
GurnikVitaliy <111244081+GurnikVitaliy@users.noreply.github.com>
2022-08-28 21:39:57 +0300

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-const block = document.querySelectorAll('.block');
-
-for(let blocks of block) {
-    blocks.addEventListener('click', function() {
-        this.classList.toggle('actv');
-    })
-}
